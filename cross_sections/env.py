6c37748176896d6a4ac3c9d5e7f38fae73b202fc
root <root@orion-196-76.crc.rice.edu>
2021-10-05 15:09:21 -0500

    vm env settings confirmed

--- cross_sections/env.py
+++ cross_sections/env.py
@@ -1,2 +1,2 @@
-#outdir='/mnt/rdf/jcm10/tetrahedra/cross_sections'
-outdir='/Volumes/research/jcm10/tetrahedra/cross_sections'
+outdir='/mnt/rdf/jcm10/tetrahedra/cross_sections'
+#outdir='/Volumes/research/jcm10/tetrahedra/cross_sections'
